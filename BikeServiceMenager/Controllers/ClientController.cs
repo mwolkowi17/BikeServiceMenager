42dc22c24ca943ca1cb967569d0703a76d8b6517
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-10-08 22:04:35 +0200

    ClientControler_added

---
+++ BikeServiceMenager/Controllers/ClientController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeServiceMenager.Data;
+using BikeServiceMenager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeServiceMenager.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var clients = _context.Clients.ToList();
+
+            var clientsToDisplay = new BikeServiceViewModel()
+            {
+                ClientList = clients
+            };
+
+            return View(clientsToDisplay);
+        }
+    }
+}
