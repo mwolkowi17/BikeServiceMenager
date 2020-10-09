ede33e100c85e61748bc3d385f3228b20be3bdf3
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-10-09 22:08:34 +0200

    BikeControlle_Added

---
+++ BikeServiceMenager/Controllers/BikeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeServiceMenager.Data;
+using BikeServiceMenager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeServiceMenager.Controllers
+{
+    public class BikeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BikeController( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var bikes = _context.Bikes
+                                .Include(n => n.Owner)
+                                .ToList();
+
+            var bikesToDislplay = new BikeServiceViewModel()
+            {
+                BikeList = bikes
+            };
+            return View(bikesToDislplay);
+        }
+    }
+}
