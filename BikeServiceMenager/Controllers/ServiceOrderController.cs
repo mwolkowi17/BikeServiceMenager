aa1d267c9f9da091c12fca5d73e5141bbb961b30
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-11-14 22:35:37 +0100

    AddOrder Controller nearly complete

--- BikeServiceMenager/Controllers/ServiceOrderController.cs
+++ BikeServiceMenager/Controllers/ServiceOrderController.cs
@@ -31,8 +31,50 @@ namespace BikeServiceMenager.Controllers
             return View(ordersToDisplay);
         }
 
-        public IActionResult AddOrder(int bikeid, string additionalinfo, int serviceactionid1, int sserviceactionid2, int serviceactionid3, int serviceactionid4, int serviceactionid5)
+        public IActionResult AddOrder(int bikeid, string additionalinfo, int serviceactionid1, int serviceactionid2, int serviceactionid3, int serviceactionid4, int serviceactionid5)
         {
+            var bikeToService = _context.Bikes
+                                .Where(n => n.BikeId == bikeid)
+                                .Include(n => n.Owner)
+                                .FirstOrDefault();
+
+            var bikeToServiceOwner = bikeToService.Owner;
+
+            var serviceAction1 = _context.ServiceActions
+                                 .Where(n => n.ServiceActionId == serviceactionid1)
+                                 .FirstOrDefault();
+
+            var serviceAction2 = _context.ServiceActions
+                                 .Where(n => n.ServiceActionId == serviceactionid2)
+                                 .FirstOrDefault();
+
+            var serviceAction3 = _context.ServiceActions
+                                 .Where(n => n.ServiceActionId == serviceactionid3)
+                                 .FirstOrDefault();
+
+            var serviceAction4 = _context.ServiceActions
+                                 .Where(n => n.ServiceActionId == serviceactionid4)
+                                 .FirstOrDefault();
+
+            var serviceAction5 = _context.ServiceActions
+                                 .Where(n => n.ServiceActionId == serviceactionid5)
+                                 .FirstOrDefault();
+
+            var newServiceOrder = new ServiceOrder()
+            {
+                BikeToService = bikeToService,
+                BikeToServiceOwner = bikeToServiceOwner,
+                DateOfOrderOpen = DateTime.Now.Date,
+                ActionToDo1 = serviceAction1,
+                ActionToDo2 = serviceAction2,
+                ActionToDo3 = serviceAction3,
+                ActionToDo4 = serviceAction4,
+                ActionToDo5 = serviceAction5,
+                AditionalNotes = additionalinfo
+            };
+
+            _context.ServiceOrders.Add(newServiceOrder);
+            _context.SaveChanges();
             return View();
         }
     }
